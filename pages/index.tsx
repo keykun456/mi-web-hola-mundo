2d1b72c9d6a9b2139d168a18f6d976b9ff17effd
keykun456 <keykun@gmail.com>
2025-07-10 18:52:12 -0600

    feat: primera version hola mundo

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white p-8">
+      <h1 className="text-5xl font-bold mb-6">¡Hola Mundo desde Next.js + Tailwind 🚀!</h1>
+      <p className="text-xl text-gray-300">¡Tu primera página web ya está viva!</p>
+    </main>
+  );
+}
